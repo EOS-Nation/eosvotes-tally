4215e0b02e420b38e36efb656d40488f7be68b9a
Denis Carriere <carriere.denis@gmail.com>
2018-11-04 09:14:18 -0500

    change to 10 min

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import { CronJob } from "cron";
 import scheduler from "./src/scheduler";
 
-// Update snapshots every 5 minutes
-const cronjob = new CronJob("*/5 * * * *", async () => {
+// Update snapshots every 10 minutes
+const cronjob = new CronJob("*/10 * * * *", async () => {
     await scheduler();
 }, undefined, true, "America/Toronto");
 
-cronjob.start();
+scheduler();
